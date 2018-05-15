4df127ecf8eee9c38f606761afab38363c7d904a
thomasFJS <thomas.fjs@eduge.ch>
2018-05-15 14:19:30 +0200

    c marche pas lol

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,20 +1,15 @@
-
-$(function(){
-	setInterval(function(){ let fourmisX = new Fourmis(); }, 3000);
-});
-
-var _role;
-var _pdv;
-var _force;
-var _defense;
-var _img;
-var _tempsCreation;
-var _cout;
-var _niveauReine;
-
-	function creerFourmi(){
+class Fourmis{
+	var _role;
+	var _pdv;
+	var _force;
+	var _defense;
+	var _img;
+	var _tempsCreation;
+	var _cout;
+	var _niveauReine;
+	constructor(){
 		this._role = "ouvriere";
-		this._pdv = 10;
+		this._pdv = 100;
 		this._force = 2;
 		this._defense = 5;
 		this._img = "#";
@@ -23,17 +18,22 @@ var _niveauReine;
 		this._niveauReine = 1;
 	}
 	
-	function __construct($role, $pdv, $force, $defense, $img, $tempsCreation, $cout, $niveauReine){
-		this._role = $role;
-		this._pdv = $pdv;
-		this._force = $force;
-		this._defense = $defense;
-		this._img = $img;
-		this._tempsCreation = $tempsCreation;
-		this._cout = $cout;
-		this._niveauReine = $niveauReine;
+	constructor(role, pdv, force, defense, img, tempsCreation, cout, niveauReine)
+	{
+		this._role = role;
+		this._pdv = pdv;
+		this._force = force;
+		this._defense = defense;
+		this._img = img;
+		this._tempsCreation = tempsCreation;
+		this._cout = cout;
+		this._niveauReine = niveauReine;
 	}
+	
+	function creerFourmi(role, pdv, force, defense, img, tempsCreation, cout, niveauReine){
+		
+	}
+	
 $(function(){
 	setInterval(function(){ let fourmisX = new Fourmis(); }, 3000);
 });
-}
