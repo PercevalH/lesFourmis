e27c7b69ceacf9ee2617327ff36891631e28d293
JulianGA <julian.gtrrz@eduge.ch>
2018-05-15 13:47:20 +0200

    J ai update les css

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>La Fourmillière</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	<header>
+	<ul id="ulHeader">
+	<li>Joueur : Pseudo</li>
+	<li>Amis</li>
+	<li>Options</li>
+	<li>Logout</li>
+	<li>Date</li>
+	</ul>
+	</header>
+	<div id="ressources">
+	<table id="tblRessources">
+		<tr><th>Nombre de Fourmis</th><th>Terre</th><th>Pierre</th><th>Nourriture</th></tr>
+		<tr><td>1</td><td>0</td><td>0</td><td>0</td></tr>		
+	</table>
+	</div>
+	<nav>
+	<ul class="list">
+	<li>Métiers</li>
+	<li>Ressources</li>
+	<li>Recherche</li>
+	<li>Camps</li>
+	</ul>
+	</nav>
+	<main>
+	
+	<h1>Bienvenue sur le jeux des fourmis vous avez une reine pour commencer !</h1>
+	
+	</main>
+	<footer>LeGroupeDesFourmis © 2018 - CFPT Informatique - Genève</footer>
+  <script src="./js/script.js"></script>
+</body>
+</html>
